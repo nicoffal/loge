77fdf7068111255ec1c81e9ca88256c53ccb207f
nicoffal <nicolas.gutierrez@offal.com.ar>
2019-05-03 10:18:54 -0300

    1
    2

---
+++ login/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="login.log" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 0px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <th>
+        social logins
+    </th>
+    <td>
+        <input type="button" id="btnGoogleLeLogin"
+               value="login with Google" class="btn btn-success" /> 
+    </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" CssClass="auto-style1" Height="35px" OnClick="Button1_Click" Text="Button" Width="203px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ login/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace login
+{
+    public partial class log : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Startup stp = new Startup();
+            //Response.Redirect("http://192.168.1.80/mesa");
+            Response.Redirect("https://accounts.google.com/signin/oauth/oauthchooseaccount?client_id=973705564392-fkmhp8564dej2j9i2pai74lfrno3j2sl.apps.googleusercontent.com&as=_arHSSUKWufr3RBoVC9wZA&destination=https%3A%2F%2Flocalhost%3A44372&approval_state=!ChRkcDlRRjhsSnY3aWpqQXBQSG5jSBIfOHdjMi1GczhNTmNhRUZvSEdUZHlJLTJOaC1XdnBSWQ%E2%88%99AJDr988AAAAAXMSlC4Hn_oDtxbTkHZpGj9zZ1bLBbAHW&oauthgdpr=1&xsrfsig=ChkAeAh8T6D8uIC-OIfHG0CVcBi4uTttUzXGEg5hcHByb3ZhbF9zdGF0ZRILZGVzdGluYXRpb24SBXNvYWN1Eg9vYXV0aHJpc2t5c2NvcGU&flowName=GeneralOAuthFlow");
+        }
+    }
+}
